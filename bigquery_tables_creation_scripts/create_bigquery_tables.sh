134da4eea9749d8db65c766c1c50b64e176782b0
viru2001 <vireshfegade@gmail.com>
2025-03-16 18:30:22 +0530

    feat: Create partitioned BigQuery table for fact table and update schemas
    Changes:
    - Update calendar_dim.json and trip_records.json with revised schemas.
    - add code to create staging tables for calendar_dim and time_dim
    - add code to create partition BigQuery table for fact table

--- bigquery_tables_creation_scripts/create_bigquery_tables.sh
+++ bigquery_tables_creation_scripts/create_bigquery_tables.sh
@@ -16,6 +16,15 @@ bq mk --table $PROJECT_ID:$DATASET_NAME.calendar_dim ./table_schemas/calendar_di
 bq mk --table $PROJECT_ID:$DATASET_NAME.fare_rate_dim ./table_schemas/fare_rate_dim.json
 bq mk --table $PROJECT_ID:$DATASET_NAME.taxi_zone_dim ./table_schemas/taxi_zone_dim.json
 bq mk --table $PROJECT_ID:$DATASET_NAME.payment_method_dim ./table_schemas/payment_method_dim.json
-bq mk --table $PROJECT_ID:$DATASET_NAME.trip_records ./table_schemas/trip_records.json
+
+
+bq mk --table $PROJECT_ID:$DATASET_NAME.time_dim_staging ./table_schemas/time_dim.json
+bq mk --table $PROJECT_ID:$DATASET_NAME.calendar_dim_staging ./table_schemas/calendar_dim.json
+
+
+bq mk --table \
+  --time_partitioning_field trip_date \
+  --time_partitioning_type DAY \
+  $PROJECT_ID:$DATASET_NAME.trip_records ./table_schemas/trip_records.json
 
 echo "All tables created successfully!"
