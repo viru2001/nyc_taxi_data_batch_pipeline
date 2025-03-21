2df98e4ee7c79dd78babba4dd0b55771a9af29ad
viru2001 <vireshfegade@gmail.com>
2025-03-21 17:30:08 +0530

    feat: truncate staging tables data after merging data into main table

--- dataflow_pipeline/merge_time_and_calender_dim.sql
+++ dataflow_pipeline/merge_time_and_calender_dim.sql
@@ -8,6 +8,7 @@ WHEN NOT MATCHED THEN
   INSERT (date_id, date, year, quarter, month, day, day_of_week)
   VALUES(S.date_id, S.date, S.year, S.quarter, S.month, S.day, S.day_of_week);
 
+truncate table nyc-taxi-batch-dataflow.trip_data.calendar_dim_staging;
 
 MERGE nyc-taxi-batch-dataflow.trip_data.time_dim T
 USING (
@@ -18,3 +19,5 @@ ON T.time_id = S.time_id
 WHEN NOT MATCHED THEN
   INSERT (time_id, hour, minute, second, period)
   VALUES(S.time_id, S.hour, S.minute, S.second, S.period);
+
+truncate table nyc-taxi-batch-dataflow.trip_data.time_dim_staging;
